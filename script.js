e4a8e235ce0fbcaec4dac39c877e7bdbf558eb57
matt <recoilmatt@gmail.com>
2019-11-21 20:50:20 -0800

    tewst

---
+++ script.js
@@ -0,0 +1,23 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+        });
+    };
+
+    //call the function
+
+    startGame();
+    updateScore():
+};
+
+
+//start the game function
+game();
